310d90b9578dbd98cffb819f2426a01d744ed748
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2020-01-28 15:02:21 +0300

    Bump to 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiocached",
-    version="0.2",
+    version="0.3",
     description="Simple decorator to cache coroutine function results",
     long_description=open('README.md', encoding='utf8').read(),
     long_description_content_type='text/markdown',
